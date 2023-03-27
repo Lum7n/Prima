e3fbae81bdcb6775adf67107fd30490bdeebfc5f
Ulla Herrmann <56406099+Lum7n@users.noreply.github.com>
2023-03-27 17:02:34 +0200

    added a character in script

--- projekt_03/Script/Build/Script.js
+++ projekt_03/Script/Build/Script.js
@@ -41,17 +41,27 @@ var Script;
     var ƒ = FudgeCore;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
+    let character;
     document.addEventListener("interactiveViewportStarted", start);
+    document.addEventListener("keydown", handleKeyboard);
     function start(_event) {
         viewport = _event.detail;
+        character = viewport.getBranch().getChildrenByName("Character")[0];
+        console.log(character);
         ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
-        // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
         let cmpCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
         viewport.camera = cmpCamera;
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
         viewport.draw();
+        // ƒ.AudioManager.default.update();
+    }
+    function handleKeyboard(_event) {
+        // console.log(_event);
+        if (_event.code == "ArrowRight" || _event.code == "KeyD")
+            character.mtxLocal.translateX(0.01);
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- projekt_03/Script/Source/Main.ts
+++ projekt_03/Script/Source/Main.ts
@@ -3,13 +3,18 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
+  let character: ƒ.Node;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+  document.addEventListener("keydown", handleKeyboard);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
+    character = viewport.getBranch().getChildrenByName("Character")[0];
+    console.log(character);
+
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
 
     let cmpCamera: ƒ.ComponentCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
     viewport.camera = cmpCamera;
@@ -19,5 +24,14 @@ namespace Script {
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
     viewport.draw();
+    // ƒ.AudioManager.default.update();
+
+  }
+
+  function handleKeyboard(_event: KeyboardEvent) {
+    // console.log(_event);
+    if (_event.code == "ArrowRight" || _event.code == "KeyD")
+    character.mtxLocal.translateX(0.01);
+
   }
 }
