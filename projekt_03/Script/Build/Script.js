28ecb17773a43d559ef13531b8f9f0c480b86060
Ulla Herrmann <56406099+Lum7n@users.noreply.github.com>
2023-03-29 19:23:52 +0200

    rabbit moves

--- projekt_03/Script/Build/Script.js
+++ projekt_03/Script/Build/Script.js
@@ -46,6 +46,7 @@ var Script;
     function start(_event) {
         viewport = _event.detail;
         character = viewport.getBranch().getChildrenByName("Character")[0];
+        console.log("Character:");
         console.log(character);
         ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
@@ -58,32 +59,12 @@ var Script;
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
             character.mtxLocal.translateX(0.05);
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]))
-            character.mtxLocal.translateX(0.05);
+            character.mtxLocal.translateX(-0.05);
         viewport.draw();
         // test
         // character.mtxLocal.translateX(0.01);
-        // viewport.draw();
+        //   viewport.draw();
         // ƒ.AudioManager.default.update();
     }
 })(Script || (Script = {}));
-// namespace Script {
-//   import ƒ = FudgeCore;
-//   ƒ.Debug.info("Main Program Template running!");
-//   let viewport: ƒ.Viewport;
-//   let character: ƒ.Node;
-//   document.addEventListener("interactiveViewportStarted", <EventListener>start);
-//   function start(_event: CustomEvent): void {
-//     viewport = _event.detail;
-//     character = viewport.getBranch().getChildrenByName("Character")[0];
-//     console.log(character);
-//     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-//     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
-//   }
-//   function update(_event: Event): void {
-//     // ƒ.Physics.simulate();  // if physics is included and used
-//     character.mtxLocal.translateX(0.01);
-//     viewport.draw();
-//     // ƒ.AudioManager.default.update();
-//   }
-// }
 //# sourceMappingURL=Script.js.map
--- projekt_03/Script/Source/Main.ts
+++ projekt_03/Script/Source/Main.ts
@@ -6,10 +6,12 @@ namespace Script {
   let character: ƒ.Node;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
+
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
     character = viewport.getBranch().getChildrenByName("Character")[0];
+    console.log("Character:")
     console.log(character);
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
@@ -27,40 +29,13 @@ namespace Script {
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
       character.mtxLocal.translateX(0.05);
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]))
-      character.mtxLocal.translateX(0.05);
+      character.mtxLocal.translateX(-0.05);
     viewport.draw();
     // test
     
     // character.mtxLocal.translateX(0.01);
-      // viewport.draw();
+    //   viewport.draw();
     // ƒ.AudioManager.default.update();
 
   }
 }
-
-// namespace Script {
-//   import ƒ = FudgeCore;
-//   ƒ.Debug.info("Main Program Template running!");
-
-//   let viewport: ƒ.Viewport;
-//   let character: ƒ.Node;
-//   document.addEventListener("interactiveViewportStarted", <EventListener>start);
-
-//   function start(_event: CustomEvent): void {
-//     viewport = _event.detail;
-
-//     character = viewport.getBranch().getChildrenByName("Character")[0];
-//     console.log(character);
-
-
-//     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-//     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
-//   }
-
-//   function update(_event: Event): void {
-//     // ƒ.Physics.simulate();  // if physics is included and used
-//     character.mtxLocal.translateX(0.01);
-//     viewport.draw();
-//     // ƒ.AudioManager.default.update();
-//   }
-// }
