8c880b57b8b311c76608238ed7f7f624c7df025d
Ulla Herrmann <56406099+Lum7n@users.noreply.github.com>
2023-04-03 17:21:46 +0200

    added animationSprite

--- projekt_03/Script/Source/Main.ts
+++ projekt_03/Script/Source/Main.ts
@@ -20,6 +20,14 @@ namespace Script {
     let cmpCamera: ƒ.ComponentCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
     viewport.camera = cmpCamera;
 
+    // // bekomme zugriff auf eine einzelne tile, Tile 1
+    // let tile: ƒ.Node = viewport.getBranch().getChildrenByName("Terrain")[0].getChildren()[1];
+    // // geo = gibt die postion der Tile in Längengrad und Breitengrad an
+    // // ohne geo gibt es den Verlauf der Achsen der Tile aus
+    // console.log(tile.mtxLocal.getX().geo.toString());
+    // // setzt die Tile wieder auf Rotation 0, 0, 0
+    // // tile.mtxLocal.rotation = ƒ.Vector3.ZERO();
+
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
@@ -37,13 +45,21 @@ namespace Script {
     ySpeed += gravity * timeFrame;
     let pos: ƒ.Vector3 = character.mtxLocal.translation;
     pos.y += ySpeed * timeFrame;
-    if (pos.y < 0.5) {
+
+    let tileCollided: ƒ.Node = checkCollision(pos);
+
+    if (tileCollided) {
       ySpeed = 0;
-      pos.y = 0.5;
+      pos.y = tileCollided.mtxWorld.translation.y + 0.5;
       isGrounded = true;
     }
+
+    // if (pos.y < 0.5) {
+    //   ySpeed = 0;
+    //   pos.y = 0.5;
+    //   isGrounded = false;
+    // }
     character.mtxLocal.translation = pos;
-    checkCollision(pos);
 
     viewport.draw();
     // ƒ.AudioManager.default.update();
@@ -53,6 +69,8 @@ namespace Script {
     let tiles: ƒ.Node[] = viewport.getBranch().getChildrenByName("Terrain")[0].getChildren()
     for (let tile of tiles) {
       let pos: ƒ.Vector3 = ƒ.Vector3.TRANSFORMATION(_posWorld,tile.mtxWorldInverse, true);
+      if (pos.x > -0.5 && pos.x < 0.5 && pos.y < 0.5)
+        return tile;
       console.log(pos);
     }
 
