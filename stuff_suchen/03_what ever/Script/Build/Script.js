897d1655bd0b698be9c9eb5a0f23f91cf0207aae
Ulla Herrmann <56406099+Lum7n@users.noreply.github.com>
2023-04-13 01:18:34 +0200

    collision failed

--- stuff_suchen/03_what ever/Script/Build/Script.js
+++ stuff_suchen/03_what ever/Script/Build/Script.js
@@ -14,21 +14,21 @@ var Script;
             if (ƒ.Project.mode == ƒ.MODE.EDITOR)
                 return;
             // Listen to this component being added to or removed from a node
-            this.addEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
-            this.addEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
-            this.addEventListener("nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */, this.hndEvent);
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
         }
         // Activate the functions of this component as response to events
         hndEvent = (_event) => {
             switch (_event.type) {
-                case "componentAdd" /* ƒ.EVENT.COMPONENT_ADD */:
+                case "componentAdd" /* COMPONENT_ADD */:
                     ƒ.Debug.log(this.message, this.node);
                     break;
-                case "componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */:
-                    this.removeEventListener("componentAdd" /* ƒ.EVENT.COMPONENT_ADD */, this.hndEvent);
-                    this.removeEventListener("componentRemove" /* ƒ.EVENT.COMPONENT_REMOVE */, this.hndEvent);
+                case "componentRemove" /* COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
                     break;
-                case "nodeDeserialized" /* ƒ.EVENT.NODE_DESERIALIZED */:
+                case "nodeDeserialized" /* NODE_DESERIALIZED */:
                     // if deserialized the node is now fully reconstructed and access to all its components and children is possible
                     break;
             }
@@ -41,38 +41,32 @@ var Script;
     var ƒ = FudgeCore;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
-    let character;
-    let gravity = -9.81;
+    let sonic;
+    const gravity = -9.81;
     let ySpeed = 0;
     let isGrounded = true;
     document.addEventListener("interactiveViewportStarted", start);
+    // document.addEventListener("keydown", hndKeyboard)
     function start(_event) {
         viewport = _event.detail;
-        character = viewport.getBranch().getChildrenByName("Character")[0];
-        console.log(character);
+        sonic = viewport.getBranch().getChildrenByName("Sonic")[0];
+        console.log(sonic);
         let cmpCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
         viewport.camera = cmpCamera;
-        // // bekomme zugriff auf eine einzelne tile, Tile 1
-        // let tile: ƒ.Node = viewport.getBranch().getChildrenByName("Terrain")[0].getChildren()[1];
-        // // geo = gibt die postion der Tile in Längengrad und Breitengrad an
-        // // ohne geo gibt es den Verlauf der Achsen der Tile aus
-        // console.log(tile.mtxLocal.getX().geo.toString());
-        // // setzt die Tile wieder auf Rotation 0, 0, 0
-        // // tile.mtxLocal.rotation = ƒ.Vector3.ZERO();
-        ƒ.Loop.addEventListener("loopFrame" /* ƒ.EVENT.LOOP_FRAME */, update);
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         let timeFrame = ƒ.Loop.timeFrameGame / 1000; // time since last frame in seconds
         // ƒ.Physics.simulate();  // if physics is included and used
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
-            character.mtxLocal.translateX(1 * timeFrame);
+            sonic.mtxLocal.translateX(1 * timeFrame);
         if (isGrounded && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])) {
             ySpeed = 5;
             isGrounded = false;
         }
         ySpeed += gravity * timeFrame;
-        let pos = character.mtxLocal.translation;
+        let pos = sonic.mtxLocal.translation;
         pos.y += ySpeed * timeFrame;
         let tileCollided = checkCollision(pos);
         if (tileCollided) {
@@ -80,17 +74,16 @@ var Script;
             pos.y = tileCollided.mtxWorld.translation.y + 0.5;
             isGrounded = true;
         }
-        character.mtxLocal.translation = pos;
+        sonic.mtxLocal.translation = pos;
         viewport.draw();
         // ƒ.AudioManager.default.update();
     }
     function checkCollision(_posWorld) {
-        let tiles = viewport.getBranch().getChildrenByName("Blocks")[0].getChildren();
+        let tiles = viewport.getBranch().getChildrenByName("Terrain")[0].getChildren();
         for (let tile of tiles) {
             let pos = ƒ.Vector3.TRANSFORMATION(_posWorld, tile.mtxWorldInverse, true);
-            if (pos.y > 0.5 && pos.x < -0.5 && pos.x < 0.5)
+            if (pos.y < 0.5 && pos.x > -0.5 && pos.x < 0.5)
                 return tile;
-            // console.log(pos);
         }
         return null;
     }
