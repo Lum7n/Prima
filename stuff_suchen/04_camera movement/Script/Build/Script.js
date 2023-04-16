982cfd775d914a13c9a0ed6c05740b9a958cb255
Ulla Herrmann <56406099+Lum7n@users.noreply.github.com>
2023-04-16 22:46:45 +0200

    added stuff

---
+++ stuff_suchen/04_camera movement/Script/Build/Script.js
@@ -0,0 +1,96 @@
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+            this.addEventListener("nodeDeserialized" /* NODE_DESERIALIZED */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+                case "nodeDeserialized" /* NODE_DESERIALIZED */:
+                    // if deserialized the node is now fully reconstructed and access to all its components and children is possible
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    let sonic;
+    const gravity = -9.81;
+    let ySpeed = 0;
+    let isGrounded = true;
+    document.addEventListener("interactiveViewportStarted", start);
+    // document.addEventListener("keydown", hndKeyboard)
+    function start(_event) {
+        viewport = _event.detail;
+        sonic = viewport.getBranch().getChildrenByName("Sonic")[0];
+        console.log(sonic);
+        let cmpCamera = viewport.getBranch().getComponent(ƒ.ComponentCamera);
+        viewport.camera = cmpCamera;
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        let timeFrame = ƒ.Loop.timeFrameGame / 1000; // time since last frame in seconds
+        // ƒ.Physics.simulate();  // if physics is included and used
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]))
+            sonic.mtxLocal.translateX(1 * timeFrame);
+        if (isGrounded && ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.SPACE])) {
+            ySpeed = 5;
+            isGrounded = false;
+        }
+        ySpeed += gravity * timeFrame;
+        let pos = sonic.mtxLocal.translation;
+        pos.y += ySpeed * timeFrame;
+        let tileCollided = checkCollision(pos);
+        if (tileCollided) {
+            ySpeed = 0;
+            pos.y = tileCollided.mtxWorld.translation.y + 0.5;
+            isGrounded = true;
+        }
+        sonic.mtxLocal.translation = pos;
+        followCamera();
+        viewport.draw();
+        // ƒ.AudioManager.default.update();
+    }
+    function checkCollision(_posWorld) {
+        let tiles = viewport.getBranch().getChildrenByName("Terrain")[0].getChildren();
+        for (let tile of tiles) {
+            let pos = ƒ.Vector3.TRANSFORMATION(_posWorld, tile.mtxWorldInverse, true);
+            if (pos.y < 0.5 && pos.x > -0.5 && pos.x < 0.5)
+                return tile;
+        }
+        return null;
+    }
+    function followCamera() {
+        let mutator = sonic.mtxLocal.getMutator();
+        viewport.camera.mtxPivot.mutate({ "translation": { "x": mutator.translation.x, "y": mutator.translation.y } });
+    }
+})(Script || (Script = {}));
+//# sourceMappingURL=Script.js.map
